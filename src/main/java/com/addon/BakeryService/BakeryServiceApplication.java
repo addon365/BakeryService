6a5641f44758e1fc9eec2347f8ee25f585e0661c
Sathish Babu <sathishbabur@hotmail.com>
2018-05-21 18:45:22 +0530

    Added My Sql DB support.

--- src/main/java/com/addon/BakeryService/BakeryServiceApplication.java
+++ src/main/java/com/addon/BakeryService/BakeryServiceApplication.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.addon.BakeryService.models.Customer;
 import com.addon.BakeryService.models.OrderItem;
@@ -25,6 +27,8 @@ import com.addon.BakeryService.models.repos.ProductRepository;
 import com.addon.BakeryService.models.repos.ShopRepository;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = { "com.addon.BakeryService.models" })
 public class BakeryServiceApplication implements CommandLineRunner {
 
 	@Autowired
@@ -45,44 +49,48 @@ public class BakeryServiceApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Customer customer = new Customer("Dummy", "9999999999", "2222222222");
-		customerRepository.save(customer);
+		// Customer customer = new Customer("Dummy", "9999999999", "2222222222");
+		// customerRepository.save(customer);
 
-		List<Shop> shops = new ArrayList<Shop>(3);
-		shops.add(new Shop("Madhura", "Madhura"));
-		shops.add(new Shop("Cauvery", "Cauvery"));
-		shops.add(new Shop("Yamunai", "Yamunai"));
-		shopRepository.saveAll(shops);
+		if (shopRepository.count() == 0) {
 
-		List<OrderStatus> orderStatuses = new ArrayList<OrderStatus>(4);
-		orderStatuses.add(new OrderStatus("Delivered", "D"));
-		orderStatuses.add(new OrderStatus("InProduction", "IP"));
-		orderStatuses.add(new OrderStatus("InStock", "IS"));
-		orderStatuses.add(new OrderStatus("Pending", "P"));
-		orderStatusRepository.saveAll(orderStatuses);
+			List<Shop> shops = new ArrayList<Shop>(3);
+			shops.add(new Shop("Madhura", "Madhura"));
+			shops.add(new Shop("Cauvery", "Cauvery"));
+			shops.add(new Shop("Yamunai", "Yamunai"));
+			shopRepository.saveAll(shops);
+		}
+		if (orderStatusRepository.count() == 0) {
+			List<OrderStatus> orderStatuses = new ArrayList<OrderStatus>(4);
+			orderStatuses.add(new OrderStatus("Delivered", "D"));
+			orderStatuses.add(new OrderStatus("InProduction", "IP"));
+			orderStatuses.add(new OrderStatus("InStock", "IS"));
+			orderStatuses.add(new OrderStatus("Pending", "P"));
+			orderStatusRepository.saveAll(orderStatuses);
+		}
+		if (productRepository.count() == 0) {
+			List<Product> products = new ArrayList<Product>(2);
+			products.add(new Product("CupCake", 10.00));
+			products.add(new Product("Bread", 25.00));
+			productRepository.saveAll(products);
 
-		List<Product> products = new ArrayList<Product>(2);
-		products.add(new Product("CupCake", 10.00));
-		products.add(new Product("Bread", 25.00));
-		productRepository.saveAll(products);
-
-		Set<OrderItem> orderItems = new HashSet<OrderItem>(2);
-		orderItems.add(new OrderItem(products.get(0), 10, 2));
-		orderItems.add(new OrderItem(products.get(1), 25, 2));
-		SalesOrder order = new SalesOrder();
-		order.setCustomer(customer);
-		order.setAdvance(200);
-		order.setTotal(0);
-		order.setOrderedDate(LocalDate.now());
-		order.setOrderedTime(LocalTime.now());
-		order.setExpectedDate(LocalDate.of(2018, 5, 15));
-		order.setExpectedTime(LocalTime.of(11, 20));
-		order.setShop(shops.get(0));
-		order.setOrderStatus(orderStatuses.get(0));
-
-		order.setOrderItems(orderItems);
-		orderRepository.save(order);
-
+		}
+		// Set<OrderItem> orderItems = new HashSet<OrderItem>(2);
+		// orderItems.add(new OrderItem(products.get(0), 10, 2));
+		// orderItems.add(new OrderItem(products.get(1), 25, 2));
+		// SalesOrder order = new SalesOrder();
+		// order.setCustomer(customer);
+		// order.setAdvance(200);
+		// order.setTotal(0);
+		// order.setOrderedDate(LocalDate.now());
+		// order.setOrderedTime(LocalTime.now());
+		// order.setExpectedDate(LocalDate.of(2018, 5, 15));
+		// order.setExpectedTime(LocalTime.of(11, 20));
+		// order.setShop(shops.get(0));
+		// order.setOrderStatus(orderStatuses.get(0));
+		//
+		// order.setOrderItems(orderItems);
+		// orderRepository.save(order);
 	}
 
 }
